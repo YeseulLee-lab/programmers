79311c02271eccb0c1229f6c91d0de615f12b234
YesMan <kaka0804@naver.com>
2025-07-26 21:43:56 +0900

    Feat: 문자열내립차순으로배치하기

---
+++ 문자열내림차순으로배치하기.cs
@@ -0,0 +1,31 @@
+﻿public class Example
+{
+    public static void Main(string[] args)
+    {
+        var sr = new StreamReader(Console.OpenStandardInput());
+        var sw = new StreamWriter(Console.OpenStandardOutput());
+
+        string str = sr.ReadLine();
+        char[] chars = str.ToArray();
+        Array.Sort(chars);
+        Array.Reverse(chars);
+
+        string upperStr = "";
+        string lowerStr = "";
+        for (int i = 0; i < chars.Length; i++)
+        {
+            if (chars[i] < 'a')
+            {
+                upperStr += chars[i];
+            }
+            else
+            {
+                lowerStr += chars[i];
+            }
+        }
+
+        sw.WriteLine(lowerStr + upperStr);
+        sw.Flush();
+    }
+
+}
