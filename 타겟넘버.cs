25a987d1e7440b1eacb828f324210af18416eea0
YesMan <kaka0804@naver.com>
2025-07-26 21:44:03 +0900

    Feat: 타겟넘버

---
+++ 타겟넘버.cs
@@ -0,0 +1,37 @@
+﻿public class Example
+{
+    public static int des;
+    public static int tar;
+
+    public static int answer = 0;
+
+    public static void Main(string[] args)
+    {
+        Console.Write(solution([4, 1, 2, 1], 2));
+    }
+
+    public static int solution(int[] numbers, int target)
+    {
+        des = numbers.Length;
+        tar = target;
+
+        DFS(0, 0, numbers);
+        return answer;
+    }
+
+    public static void DFS(int sum, int n, int[] arr)
+    {
+        if (n < des)
+        {
+            DFS(sum + arr[n], n + 1, arr);//노드를 돌면서 더하고 빼면서 target이랑 같은지 확인
+            DFS(sum - arr[n], n + 1, arr);
+        }
+        else
+        {
+            if (sum == tar)
+            {
+                answer ++;
+            }
+        }
+    }
+}
