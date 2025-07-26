f8930e9710d33b885d4802bdd4ee008af8a515e9
YesMan <kaka0804@naver.com>
2025-07-26 21:43:44 +0900

    Feat: 나머지1이되는수찾기

---
+++ 나머지1이되는수찾기.cs
@@ -0,0 +1,23 @@
+﻿public class Example
+{
+    public static void Main(string[] args)
+    {
+        var sr = new StreamReader(Console.OpenStandardInput());
+        var sw = new StreamWriter(Console.OpenStandardOutput());
+
+        int n = int.Parse(sr.ReadLine());
+
+        int result = 0;
+        for (int i = 2; i < n; i++)
+        {
+            if ((n % i) == 1)
+            {
+                result = i;
+                break;
+            }
+        }
+
+        sw.WriteLine(result);
+        sw.Flush();
+    }
+}
